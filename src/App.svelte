f8ad55282e93d9a6b2868e6bf087dee5e002e3be
humberto2626 <holabalancedog@gmail.com>
2025-09-18 08:43:06 -0400

    Updated App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1167,8 +1167,8 @@ This is when the timer starts."
   .golden-bone-active-illustration {
     display: block;
     margin: 1rem;
-    width: 100px;
-    height: 100px;
+    width: 40px;
+    height: 40px;
     object-fit: contain;
     filter: drop-shadow(0 0 10px #ffd700);
   }
