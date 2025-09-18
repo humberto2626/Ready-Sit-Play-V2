9fa4f6b492a3eca56410cb3c6ec00b3ecebcc956
humberto2626 <holabalancedog@gmail.com>
2025-09-18 08:21:14 -0400

    Updated App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1169,7 +1169,7 @@ This is when the timer starts."
     margin: 1rem;
     width: 200px;
     height: 200px;
-    object-fit: center;
+    object-fit: flex;
     filter: drop-shadow(0 0 10px #ffd700);
   }
 
