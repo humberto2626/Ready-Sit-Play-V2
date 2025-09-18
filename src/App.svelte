a9c3869c4ce3d4a3e2178c339e21e1ff5296dbae
humberto2626 <holabalancedog@gmail.com>
2025-09-18 13:19:56 -0400

    Updated App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -111,90 +111,90 @@
   let actionTooltipCard: Card | null = null;
 
   const actionInstructions: Record<string, string> = {
-    'Recall': `Call ${dogName || 'your dog'} from as far as possible into a treat in your extended hand.`,
-    'Sit': `With a treat in your hand, grace just above ${dogName || 'your dog'}'s nose until they sit.`,
-    'Greet': `Present your fist with a treat inside, if ${dogName || 'your dog'} sniffs, licks or nudges your hand, reward their good manners.`,
-    'Heel': `With a treat in your hand, guide ${dogName || 'your dog'} to walk right next to you for at least five steps.`,
-    'Focus': `Holding a treat between your index finger and thumb, grace just above ${dogName || 'your dog'}'s nose and then place it between your eyebrows, count out loud for at least three seconds.`,
-    'Paw': `With a treat in your hand, grace and hold just below the ear of ${dogName || 'your dog'}, wait for them to use their paw to push off your hand.`,
-    'Down': `With a treat in your hand, grace ${dogName || 'your dog'}'s chin and chest as you place your hand flat on the ground in between their front legs, waiting for them to lay down.`,
-    'Back': `With ${dogName || 'your dog'} sitting, hold a treat in your hand just above the top of ${dogName || 'your dog'}'s head, putting your foot in between their front paws, move your hand towards their tail, waiting for them to move back.`,
-    'Stay': `With ${dogName || 'your dog'} sitting or laying down, show the palm of your hand and slowly take at least three steps backwards, return and reward ${dogName || 'your dog'}'s self-control.`,
-    'Place': `With a towel on the floor, guide ${dogName || 'your dog'} near it and drop a treat in the towel the moment they step on it, praise and reward them again.`
+   'Recall': `Call the canine player from as far as possible into a treat in your extended hand.`,
+    'Sit': `With a treat in your hand, grace just above the canine player nose until they sit.`,
+    'Greet': `Present your fist with a treat inside, if the canine player sniffs, licks or nudges your hand, reward their good manners.`,
+    'Heel': `With a treat in your hand, guide the canine player to walk right next to you for at least five steps.`,
+    'Focus': `Holding a treat between your index finger and thumb, grace just above the canine player nose and then place it between your eyebrows, count out loud for at least three seconds.`,
+    'Paw': `With a treat in your hand, grace and hold just below the ear of the canine player, wait for them to use their paw to push off your hand.`,
+    'Down': `With a treat in your hand, grace the canine player chin and chest as you place your hand flat on the ground in between their front legs, waiting for them to lay down.`,
+    'Back': `With the canine player sitting, hold a treat in your hand just above the top of the canine player head, putting your foot in between their front paws, move your hand towards their tail, waiting for them to move back.`,
+    'Stay': `With the canine player sitting or laying down, show the palm of your hand and slowly take at least three steps backwards, return and reward the canine player self-control.`,
+    'Place': `With a towel on the floor, guide the canine player near it and drop a treat in the towel the moment they step on it, praise and reward them again.`
+
   };
 
   const challengeInstructions: Record<string, string> = {
     'Squirrel': 'Toss a ball to create a distraction',
     'Do it Twice': 'Self explanatory',
-    'Cat Ate Your Tongue': 'Guide your dog to perform the Action Card without saying a word',
-    'Pirate Leg': 'Guide your dog to perform the Action Card while standing only on one leg',
-    'Hands Behind Your Back': 'Guide your dog to perform the Action Card without any hand gestures',
-    'Long Distance Call': `Guide your dog to perform the Action Card standing at least five steps away from them`
+    'Cat Ate Your Tongue': 'Guide the canine player to perform the Action Card without saying a word',
+    'Pirate Leg': 'Guide the canine player to perform the Action Card while standing only on one leg',
+    'Hands Behind Your Back': 'Guide the canine player to perform the Action Card without any hand gestures',
+    'Long Distance Call': `Guide the canine player to perform the Action Card standing at least five steps away from them`
   };
   // Instructions walkthrough state
   const miniGameExplanations: Record<string, string> = {
     'Two Ball Fetch': `
       1. Win by: Getting more balls fetched under 30 seconds.
-      2. Set Up: You get the two balls on your turn, the timer starts the moment you toss the first ball.
-*to count the throw has to be at least three times the length of ${dogName || 'your dog'}.
-*to count the fetch ${dogName || 'your dog'} has to come to where you can pick up the ball without moving one of your feet.
-      3. How to Play: While keeping one foot glued where you are standing, throw the first ball, once ${dogName || 'your dog'} brings it back, show the second one, when ${dogName || 'your dog'} drops it, throw the second one.
-      4. Tie Breaker: Each player has a ball, standing next to each other you both throw it at the same time, the ball that gets fetched wins.
+      2. Set Up: You get the two balls on your turn, the timer starts the moment you toss the first one.
+*The throw has to be at least three times the length of the canine player.
+*For the fetch to count the canine player has to come to where you can pick up the ball without moving one of your feet.
+      3. How to Play: While keeping one foot glued where you are standing, throw the first ball, once the canine player brings it back, show the second one, when the canine player drops the first ball, throw the second one.
+      4. Tie Breaker: Each player has a ball, standing next to each other, both throw it at the same time, the ball that gets fetched wins.
     `,
     'Sniff and Choose': `
-      1. Win by: Having ${dogName || 'your dog'} choose your hand over your opponent's.
+      1. Win by: Having the canine player choose your hand over your opponent's.
       2. Set Up: Each player hides a treat in their closed fist. Both players kneel down facing each other about 3 feet apart.
-      3. How to Play: Present both closed fists to ${dogName || 'your dog'} simultaneously. ${dogName || 'Your dog'} will sniff and choose one fist by pawing, licking, or nudging it.
-      4. Tie Breaker: N/A
+      3. How to Play: Present both closed fists to the canine player simultaneously. The canine player will sniff and choose one fist by pawing, licking, or nudging it.
     `,
     'Hot Potato': `
-      1. Win by: Not having the ball in your hands when the 30 seconds run out.
-      2. Set Up: Players stand on opposite sides of the room you're playing in, one of them holding a ball.
-      3. How to Play: The player that's empty handed calls ${dogName || 'your dog'}'s name, only then the player that has the ball passes it, the player receiving the ball lowers it in such a way that ${dogName || 'your dog'} comes closer to take it, only then the player that is now empty handed calls ${dogName || 'your dog'}'s name to continue playing.
+      1. Win by: Not having the ball in your hands when the 30 second timer runs out.
+      2. Set Up: Players stand on opposite sides of the room, one of them holding a ball.
+      3. How to Play: The player that's empty handed calls the canine player's name, only then the player that has the ball passes it, the player receiving the ball lowers it in such a way that the canine player comes closer to take it, only then the player that is now empty handed calls the canine player's name to continue playing.
       4. Tie Breaker: In the event of the ball being mid air when the timer goes off, the game is repeated.
     `,
     'Hide and Seek': `
-      1. Win by: If hiding, not being found, if seeking, having ${dogName || 'your dog'} finding the other player under 30 seconds.
-      2. Set Up: "The player that picked the card hides while the other player takes ${dogName || 'your dog'} to a different room and asks them to "Sit" for a count of 10 out loud.
-This is when the timer starts. 
-      3. How to Play: The player holding ${dogName || 'your dog'} releases them and says "Find (Name of the player)!" ${dogName || 'Your dog'} will search for the hidden player.
-      4. Tie Breaker: N/A
+      1. Win by: If hiding, not being found, if seeking, having the canine player find the other player under 30 seconds.
+      2. Set Up: "The player that picked up the card hides, while the other player takes the canine player to a different room, asks them to "Sit" and counts out loud to 10.
+      3. How to Play: The timer begins when the player holding the canine player releases them and says "Find (Name of the hidden player)!" the canine player will search for the hidden player.
     `,
     'The Statue': `
-      1. Win by: Keeping ${dogName || 'your dog'} sitting the longest time possible up to 30 seconds.
-      2. Set Up: Each player positions ${dogName || 'your dog'} in a "Sit" about 3 feet away from them, the timer starts the moment ${dogName || 'your dog'}'s bottom touches the floor.
-      3. How to Play: Each player gets a 30 second turn, in which as ${dogName || 'your dog'} sits you must keep the count out loud.
-      4. Tie Breaker: if both players reach the exact same count, take a ball and repeat the game, but this time, as you count you must also bounce the ball.
+      1. Win by: Keeping the canine player sitting the longest time possible up to 30 seconds.
+      2. Set Up: Each player positions the canine player in a "Sit" about 3 feet away from them, the timer starts the moment the canine player's bottom touches the floor.
+      3. How to Play: Each player gets a 30 second turn, as the canine player sits the human players count out loud the seconds.
+      4. Tie Breaker: if both players reach the exact same count, repeat the game, but this time, the player who is not counting takes a ball and bounces it for each second the canine player remains seated.
     `,
     'Pawathon': `
-      1. Win by: Being the player to whom ${dogName || 'your dog'} gives the most paws under 30 seconds.
-      2. Set Up: Players take turns standing in front of ${dogName || 'your dog'} to offer their hand repeatedly without rewarding until the end of the 30 seconds. *Both players reward at the end of their turn as to not discourage ${dogName || 'your dog'}.
-      3. How to Play: ${dogName || 'Your dog'} sits and each player asks for paw repeatedly. *only one paw at a time, double paws or high 10s don't count.
-      4. Tie Breaker: Both players stand in front of the sitting dog, they both ask for Paw at the same time. whoever gets the paw wins.
+      1. Win by: Being the player to whom the canine player gives the most paws under 30 seconds.
+      2. Set Up: Players take turns standing in front of the canine player to offer their hand repeatedly without reward until the end of the 30 seconds. 
+*Both players reward at the end of their turn as to not discourage the canine player.
+      3. How to Play: the canine player sits and each player asks for paw repeatedly. 
+*only one paw at a time, double paws or high 10s don't count.
+      4. Tie Breaker: Both players stand in front of the seated canina player , they both ask for Paw at the same time. whoever gets the paw wins.
     `,
     'Treasure Hunt': `
-      1. Win by: If hiding the treat, not having it found, if hunting, having ${dogName || 'your dog'} finding the treat under 30 seconds.
-      2. Set Up: "The player that picked the card hides the treat while the other player takes ${dogName || 'your dog'} to a different room and asks them to Sit for a count of 10 out loud.
-This is when the timer starts."
-      3. How to Play: Release ${dogName || 'your dog'} and give them the "find it" command. ${dogName || 'Your dog'} will search for the hidden treats.
-      4. Tie Breaker: N/A
+      1. Win by: If hiding the treat, not having it found, if hunting, having the canine player find the treat under 30 seconds.
+      2. Set Up: "The player that picked the card hides the treat while the other player takes the canine player to a different room, asks them to "Sit" and counts out loud to 10.
+
+      3. How to Play: The timer begins when the player holding the canine player releases them and says "find it" the canine player will search for the hidden treats.
     `,
     'Dog Roll': `
-      1. Win by: Getting ${dogName || 'your dog'} to complete the most rolls in a row under 30 seconds.
-      2. Set Up: Each player positions ${dogName || 'your dog'} in the "Down" position on a soft surface like grass or carpet.
-      3. How to Play: Players take turns commanding ${dogName || 'your dog'} to "roll over." Count consecutive successful rolls until ${dogName || 'your dog'} stops or gets up.
-      4. Tie Breaker: If both dogs achieve the same number of rolls, go into a Roll face off, where you take turns asking ${dogName || 'your dog'} to roll in sudden death format.
+      1. Win by: Getting the canine player to complete the most rolls in a row under 30 seconds.
+      2. Set Up: Each player positions the canine player in the "Down" position on a soft surface like grass or carpet.
+      3. How to Play: Each player guides the canine player to "Roll Over." Count consecutive successful rolls until the canine player stops or gets up.
+      4. Tie Breaker: If both dogs achieve the same number of rolls, go into a “Roll face off”, where you take turns guiding the canine player to roll in sudden death format.
     `,
     'The Catcher': `
-      1. Win by: Having ${dogName || 'your dog'} catch the most treats out of 5 tosses.
-      2. Set Up: Each player stands 2 long steps away from ${dogName || 'your dog'} with 5 small treats. Players take turns.
-      3. How to Play: Toss treats one at a time gently toward ${dogName || 'your dog'}'s mouth. Count successful catches. After both players complete their 5 tosses, compare scores.
+      1. Win by: Having the canine player catch the most treats out of 5 tosses.
+      2. Set Up: Each player stands 2 long steps away from the canine player with 5 small treats. 
+      3. How to Play: Toss each treat gently toward the canine player's mouth. Count successful catches. After both players complete their 5 tosses, compare scores.
       4. Tie Breaker: If tied, each player gets 3 additional tosses in sudden death format.
     `,
      'Give Me, Drop it, Leave it!': `
-      1. Win by: Collecting the most points from  ${dogName || 'your dog'} giving you, dropping or leaving a toy under 30 seconds.
-      2. Set Up: Each player starts by holding the toy in front of ${dogName || 'your dog'}. Players take turns and ask the canine player to "Give me" for 1 point, "Drop it" 2 points and "Leave it" for 3 points.
-      3. How to Play: Toss the toy, after each time ${dogName || 'your dog'} performs one of the behaviors, toss it again, Count successful behaviors. After all players complete their turns, compare scores.
+      1. Win by: Collecting the most points from the canine player giving you, dropping or leaving a toy under 30 seconds.
+      2. Set Up: Each player starts by holding the toy in front of the canine player. 
+      3. How to Play: Toss the toy, after each time the canine player performs one of the behaviors, toss it again. 
+Each player asks the canine player to "Give me" for 1 point, "Drop it" 2 points and "Leave it" for 3 points.
       4. Tie Breaker: If tied, each player gets 3 additional tosses in sudden death format.
     `
   };
