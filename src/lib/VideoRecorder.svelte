1f81fa25c444d39b2d81faeeb10e53c8b9ac19ea
humberto2626 <holabalancedog@gmail.com>
2025-09-23 11:14:46 -0400

    Remove duplicate countdown display

--- src/lib/VideoRecorder.svelte
+++ src/lib/VideoRecorder.svelte
@@ -375,9 +375,6 @@
         <div class="countdown-display">
           {countdown}s
         </div>
-        <div class="countdown-display">
-          {countdown}s
-        </div>
         <button class="stop-btn-container" on:click={stopRecording}>
           <div class="stop-btn-circle">
             <div class="stop-btn-square"></div>
