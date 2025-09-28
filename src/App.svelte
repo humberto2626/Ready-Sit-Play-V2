46d37d0233c0914b855ead7eba0bf2d5e06c3a34
humberto2626 <holabalancedog@gmail.com>
2025-09-28 16:41:37 -0400

    Updated App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2078,8 +2078,8 @@ Each player asks the canine player to "Give me" for 1 point, "Drop it" 2 points 
         </div>
       {/if}
 
-      <h3>{player1Name || 'Player 1'}'s Cards 
-        {player1Cards.filter(c => c.category === 'Action').length}</h3>
+      <h3>{player1Name || 'Player 1'}'s Cards</h3>
+        <h3>{player1Cards.filter(c => c.category === 'Action').length}</h3>
 
       <div class="player-cards-container" class:compact-display={player1Cards.length > 5}>
         <!-- Top row: Challenge and Mini Game cards (non-action) with click for Challenge -->
