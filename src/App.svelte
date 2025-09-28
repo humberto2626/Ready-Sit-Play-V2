8b3c54d563406c8a83a5eccef0107a60faeab9dc
humberto2626 <holabalancedog@gmail.com>
2025-09-28 19:29:54 -0400

    Updated App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -714,9 +714,7 @@ Each player asks the canine player to "Give me" for 1 point, "Drop it" 2 points 
   }
 
   async function actionCompleted() {
-    if (!activeCard || activeCard.category 
-    )
-  }!== 'Action' || gameOver) return;
+    if (!activeCard || activeCard.category!== 'Action' || gameOver) return;
 
     saveCurrentState();
 
