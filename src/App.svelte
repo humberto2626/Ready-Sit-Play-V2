8c21d9a91b36108f2c33df6fa5869a35c4950de8
humberto2626 <holabalancedog@gmail.com>
2025-10-01 14:46:53 -0400

    Updated App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2030,7 +2030,7 @@ Each player asks the canine player to "Give me" for 1 point, "Drop it" 2 points 
 <div class="deck-area">
   <!-- Menu Icon Button -->
   <button class="menu-icon-btn" onclick={toggleMenuOverlay}>
-    <h2>||</h2>
+    <h1>||</h1>
   </button>
 
   <!-- Global Timer Display -->
